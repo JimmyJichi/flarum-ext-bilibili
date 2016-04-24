dea1b2151ebd673ff35e56527f9f41ff0c5352ed
张骥驰 <zhangjichide@gmail.com>
2016-04-25 06:01:47 +0800

    Adjust width

--- bootstrap.php
+++ bootstrap.php
@@ -1,46 +1,29 @@
-<?php namespace JJaicmkmy\Flarum\MediaEmbed;
+<?php namespace JJaicmkmy\Bilibili;
 
-use Flarum\Events\FormatterConfigurator;
-use Flarum\Support\Extension as BaseExtension;
+use Flarum\Event\ConfigureFormatter;
 use Illuminate\Events\Dispatcher;
-use s9e\TextFormatter\Configurator\Bundles\MediaPack;
 
-class Extension extends BaseExtension
+function subscribe(Dispatcher $events)
 {
-    public function listen(Dispatcher $events)
-    {
-        $events->subscribe(__NAMESPACE__ . '\\Listener');
-    }
-}
-
-class Listener
-{
-    public function subscribe(Dispatcher $events)
-    {
-        $events->listen('Flarum\\Events\\FormatterConfigurator', [$this, 'addMediaSites']);
-    }
-
-    public function addMediaSites(FormatterConfigurator $event)
-    {
-        $event->configurator->templateChecker->remove('DisallowUnsafeDynamicCSS');
-        $event->configurator->MediaEmbed->enableResponsiveEmbeds();
-
-
-$event->configurator->MediaEmbed->add(
+	$events->listen(
+		ConfigureFormatter::class,
+		function (ConfigureFormatter $event)
+		{
+			$event->configurator->MediaEmbed->add(
 'bilibili',
 [
     'host'    => 'www.bilibili.com',
     'extract' => "!www.bilibili.com/video/av(?'id'[0-9]+)/!",
-    'iframe'  => [
-        'width'  => 720,
-        'height' => 405,
-        'src'    => 'https://bilidown.tlo.xyz/embed.php/{@id}.mp4'
+    'flash'  => [
+        'width'  => 640,
+        'height' => 360,
+        'src'    => 'https://static-s.bilibili.com/miniloader.swf?aid={@id}'
     ]
 ]
 );
 
-        (new MediaPack)->configure($event->configurator);
-    }
-}
+		}
+	);
+};
 
-return __NAMESPACE__ . '\\Extension';
+return __NAMESPACE__ . '\\subscribe';
